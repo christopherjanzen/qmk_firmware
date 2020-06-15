9f771ad2c809512a17641f26002270ec75e1db75
Christopher Janzen <chrisisaakjanzen@gmail.com>
2020-06-14 21:33:13 -0500

    Switch tab to layer mod to access FN1 layer. Switch RALT to Momentary FN3 layer.

--- keyboards/cannonkeys/tmov2/keymaps/cijanzen/keymap.c
+++ keyboards/cannonkeys/tmov2/keymaps/cijanzen/keymap.c
@@ -31,10 +31,10 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_BASE] = LAYOUT_default(
-		KC_GESC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
+		KC_GESC, LT(_FN1, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
 		DM_REC1, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
 		DM_RSTP, KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FN2),
-		DM_PLY1, KC_LGUI, KC_LALT, KC_TRNS,                  KC_SPC,                  KC_TRNS, MO(_FN1)
+		DM_PLY1, KC_LGUI, KC_LALT, KC_TRNS,                  KC_SPC,                  KC_TRNS, MO(_FN3)
     ),
 
     [_FN1] = LAYOUT_default(
